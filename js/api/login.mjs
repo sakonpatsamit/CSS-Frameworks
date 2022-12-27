15bbf475b673746c9927c6582da63e3d59f7848e
sakonpatsamit <95245596+sakonpatsamit@users.noreply.github.com>
2022-12-27 12:31:23 +0100

    created api login function

--- js/api/login.mjs
+++ js/api/login.mjs
@@ -1,10 +1,7 @@
 import { API_AUTH_LOGIN } from "./constants.mjs";
 
-function userLogin (event) {
-event.preventDefault();
-const loginEmail = document.querySelector("#loginEmail").value 
-const loginPassword = document.querySelector("#loginPassword").value
 
+export function loginHandler(loginEmail, loginPassword) {
 var myHeaders = new Headers();
 myHeaders.append("Content-Type", "application/json");
 
@@ -22,7 +19,29 @@ var requestOptions = {
 
 fetch(`${API_AUTH_LOGIN}`, requestOptions)
   .then(response => response.json())
-  .then(result => console.log(result))
+  .then(result => {
+  if (result.accessToken == undefined) {
+    alert(result.errors.message);
+    return;
+  }
+  const token = result.accessToken;
+  const userName = result.name
+  localStorage.setItem("token", accessId);
+  localStorage.setItem("username", userName);
+  document.location = "./home.html";
+  })
   .catch(error => console.log('error', error));
+}
+
+export function loginInit(event) {
+    event.preventDefault();
+    const loginEmail = document.querySelector("#loginEmail").value.toLowerCase() 
+    const loginPassword = document.querySelector("#loginPassword").value
+    
+    if (!loginEmail || !loginPassword) {
+        alert("Please enter user email and password.");
+        return;
+    }
+    loginHandler(loginEmail, loginPassword);
 }
 
